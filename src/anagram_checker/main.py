b0759ed14d0e525d94d3e6e5983edd6a776776a4
Tobias Knuth <mail@tobiasknuth.de>
2021-03-02 11:16:48 +0100

    tick_for, tick_while

--- src/anagram_checker/main.py
+++ src/anagram_checker/main.py
@@ -1,2 +1,44 @@
-def tick():
+def tick_while(a, b):
+    if len(a) != len(b):
+        return False
+
+    a = list(a)
+    i = 0
+    status = True
+    while i < len(b) and status:
+        j = 0
+        found = False
+        while j < len(a) and not found:
+            if b[i] == a[j]:
+                found = True
+            else:
+                j += 1
+        if found:
+            a[j] = None
+        else:
+            status = False
+        i += 1
+
+    return status
+
+
+def tick_for(a, b):
+    if len(a) != len(b):
+        return False
+
+    a = list(a)
+    for letter in b:
+        for i in range(len(a)):
+            if letter == a[i]:
+                a[i] = None
+
+    for letter in a:
+        if letter is not None:
+            return False
+
     return True
+
+
+def brute_force(a, b):
+    # possible_words = []
+    pass
--- tests/test_anagrams.py
+++ tests/test_anagrams.py
@@ -1,5 +1,28 @@
-from anagram_checker import tick
+from anagram_checker import tick_while, tick_for, brute_force
+import pytest
+
+tuples = (
+    ("lager", "regal", True),
+    ("lagern", "regal", False),
+    ("lager", "regale", False),
+    ("lager", "nagel", False),
+    ("biere", "reibe", True),
+)
 
 
-def test_tick():
-    tick()
+@pytest.mark.parametrize("tuple", tuples)
+def test_tick_while(tuple):
+    a, b, is_anagram = tuple
+    assert tick_while(a, b) == is_anagram
+
+
+@pytest.mark.parametrize("tuple", tuples)
+def test_tick_for(tuple):
+    a, b, is_anagram = tuple
+    assert tick_for(a, b) == is_anagram
+
+
+@pytest.mark.parametrize("tuple", tuples)
+def test_brute_force(tuple):
+    a, b, is_anagram = tuple
+    # assert brute_force(a, b) == is_anagram
